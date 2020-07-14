4d5093a9a3cba83058d9dd2db0ebc58186aa7c80
Kevin McJunkin <kevin.mcjunkin@rackspace.com>
2020-07-14 16:52:28 -0500

    Moving modules out to be global

--- bootstrap/centos/7_new/hotstrap.py
+++ bootstrap/centos/7_new/hotstrap.py
@@ -15,10 +15,6 @@ def pip_down():
                'os-apply-config',
                'os-collect-config',
                'os-refresh-config',
-               'dib-utils',
-               '-U decorator',
-               'ansible==2.4.3.0',
-               # '-U git+https://github.com/kmcjunk/os-refresh-config.git'
                ]
     try:
         for package in os_list:
